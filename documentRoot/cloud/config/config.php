bb922abc9872043e8946d19033b9c9c2cc31f769
marinagr17 <mgracia1411@gmail.com>
2025-11-12 19:39:50 +0000

    config

---
+++ documentRoot/cloud/config/config.php
@@ -0,0 +1,21 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'occkqurkard5',
+  'passwordsalt' => 'AhVRIEd+/siAm0kcDtrQZtCHleQKYD',
+  'secret' => 'givPvBuAZoo8Z4x4JV8ChRxJtgrh7EBz0o2Egw6wc6kRzIcD',
+  'trusted_domains' => 
+  array (
+    0 => 'cloud.marina.org',
+  ),
+  'datadirectory' => '/var/www/cloud/data',
+  'dbtype' => 'mysql',
+  'version' => '32.0.1.2',
+  'overwrite.cli.url' => 'http://cloud.marina.org',
+  'dbname' => 'nextcloud',
+  'dbhost' => '10.0.3.50',
+  'dbtableprefix' => 'oc_',
+  'mysql.utf8mb4' => true,
+  'dbuser' => 'nextcloud',
+  'dbpassword' => 'marina123nc',
+  'installed' => true,
+);
